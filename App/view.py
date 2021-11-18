eff75f13a2739729ab79257dcd1277134c696673
Pau3 <p.dazad@uniandes.edu.co>
2021-11-17 21:24:55 -0500

    Primera entrega – Reto 4

--- App/view.py
+++ App/view.py
@@ -18,41 +18,97 @@
  *
  * You should have received a copy of the GNU General Public License
  * along withthis program.  If not, see <http://www.gnu.org/licenses/>.
- """
+"""
 
-import config as cf
+
 import sys
-import controller
-from DISClib.ADT import list as lt
-assert cf
-
+import config
+import threading
+import time
+from App import controller
+from DISClib.ADT import stack
+assert config
 
 """
-La vista se encarga de la interacción con el usuario
-Presenta el menu de opciones y por cada seleccion
-se hace la solicitud al controlador para ejecutar la
-operación solicitada
+La vista se encarga de la interacción con el usuario.
+Presenta el menu de opciones  y  por cada seleccion
+hace la solicitud al controlador para ejecutar la
+operación seleccionada.
 """
+
+# ___________________________________________________
+#  Variables
+# ___________________________________________________
+
+
+
+
+# ___________________________________________________
+#  Menu principal
+# ___________________________________________________
+
 
 def printMenu():
+    print("\n")
+    print("*******************************************")
     print("Bienvenido")
-    print("1- Cargar información en el catálogo")
-    print("2- ")
+    print("1- Cargar archivos")
+    print("2- Crear catálogo")
+    print("3- Encontrar puntos de interconexión aérea")
+    print("4- Encontrar clústeres de tráfico aéreo")
+    print("5- Encontrar la ruta más corta entre ciudades")
+    print("6- Utilizar las millas de viajero ")
+    print("7- Cuantificar el efecto de un aeropuerto cerrado ")
+    print("8- Comparar con servicio WEB externo")
+    print("9- Visualizar gráficamente los requerimientos")
+    print("0- Salir")
+    print("*******************************************")
 
-catalog = None
+
+def optionOne(cont):
+    print("\nCargando información de transporte de singapur ....")
+    start_time = time.process_time()
+    print("\nLa operación tardó ", elapsed_time_ms, " ms.")
 
 """
 Menu principal
 """
-while True:
-    printMenu()
-    inputs = input('Seleccione una opción para continuar\n')
-    if int(inputs[0]) == 1:
-        print("Cargando información de los archivos ....")
 
-    elif int(inputs[0]) == 2:
-        pass
 
-    else:
-        sys.exit(0)
-sys.exit(0)
+def thread_cycle():
+    while True:
+        printMenu()
+        inputs = input('Seleccione una opción para continuar\n>')
+
+        if int(inputs[0]) == 1:
+            optionOne(cont)
+
+        elif int(inputs[0]) == 2:
+            optionTwo(cont)
+
+        elif int(inputs[0]) == 3:
+            optionThree(cont)
+
+        elif int(inputs[0]) == 4:
+            optionFour(cont)
+
+        elif int(inputs[0]) == 5:
+            optionFive(cont)
+
+        elif int(inputs[0]) == 6:
+            optionSix(cont)
+
+        elif int(inputs[0]) == 7:
+            optionSeven(cont)
+
+        else:
+            sys.exit(0)
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    threading.stack_size(67108864)  # 64MB stack
+    sys.setrecursionlimit(2 ** 20)
+    thread = threading.Thread(target=thread_cycle)
+    thread.start()
+
